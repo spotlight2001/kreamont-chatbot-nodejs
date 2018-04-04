fb8b7cc11774c40b828b8faaccd24090abc31fcc
spotlight2001 <waltermauritz@gmx.at>
2018-04-04 20:11:59 +0200

    calendar with private/public url

--- app/intent-calendar.service.js
+++ app/intent-calendar.service.js
@@ -5,17 +5,20 @@ const http = require("request-promise-native");
 const moment = require('moment');
 const userService = require('./user.service');
 
-exports.fulfill = function (request, sendResponse) {
-    let url = undefined;
-    let user = userService.getUser(request);
-
+function getUrl(calendarId) {
     // 2018-02-26T00:00:00+01:00
     let dateParam = moment().startOf('day');
     dateParam = dateParam.format("YYYY-MM-DD[T]00:00:00+01:00");
     dateParam = encodeURIComponent(dateParam);
+    let url = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events?key=AIzaSyAQt8f07E4UQINLoYjaqMpegyWs8BzgeUg&maxResults=3&orderBy=startTime&singleEvents=true&timeMin=' + dateParam;
+    return url;
+}
 
-    let publicCalendarPromise = http.get({ json: true, uri: 'https://clients6.google.com/calendar/v3/calendars/32kbr4dsdljsqgpsromm2det5c@group.calendar.google.com/events?singleEvents=true&timeZone=Europe/Vienna&maxResults=3&sanitizeHtml=true&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&timeMin=' + dateParam });
-    let privateCalendarPromise = http.get({ json: true, uri: 'https://clients6.google.com/calendar/v3/calendars/4gvh2mvg0lh96d1o0g0k19jon4@group.calendar.google.com/events?singleEvents=true&timeZone=Europe/Vienna&maxResults=3&sanitizeHtml=true&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&timeMin=' + dateParam });
+exports.fulfill = function (request, sendResponse) {
+    let url = undefined;
+    let user = userService.getUser(request);
+    let publicCalendarPromise = http.get({ json: true, uri: getUrl('32kbr4dsdljsqgpsromm2det5c@group.calendar.google.com') });
+    let privateCalendarPromise = http.get({ json: true, uri: getUrl('4gvh2mvg0lh96d1o0g0k19jon4@group.calendar.google.com') });
     let promises = [publicCalendarPromise];
     if (user && user.isKreamont) {
         promises.push(privateCalendarPromise);
@@ -23,21 +26,21 @@ exports.fulfill = function (request, sendResponse) {
 
     Promise.all([publicCalendarPromise, privateCalendarPromise]).then(function (calendars) {
         let appointments = [];
-        for(let calendar of calendars) {
+        for (let calendar of calendars) {
             for (let appointment of calendar.items) {
                 appointments.push(appointment);
             }
         }
         appointments = _.sortBy(appointments, ['start.date']);
-        appointments = appointments.slice(0,3);
+        appointments = appointments.slice(0, 3);
         console.log('got appointments: ' + appointments.length);
         let result = 'Die nächsten Termine: \n';
-        for(let appointment of appointments) {
+        for (let appointment of appointments) {
             console.log(appointment.start.date);
             let date = appointment.start.date;
             let title = appointment.summary;
             result += date + ' ' + title + '\n';
-        }        
+        }
         sendResponse(result);
         for (let i = 0; i < response.items.length; i++) {
 
