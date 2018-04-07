963e36edd21d19df08af0f938b8c6b9629bb24ff
spotlight2001 <waltermauritz@gmx.at>
2018-04-07 08:09:28 +0200

    try a card

--- app/intent-news.service.js
+++ app/intent-news.service.js
@@ -12,16 +12,32 @@ exports.fulfill = function (sendResponse) {
         let info = news[0];
         let result = {
             fulfillmentMessages: [{
-                platform: 'ACTIONS_ON_GOOGLE',
-                text: {
-                    text: [info.title.rendered]
+                'platform': 'ACTIONS_ON_GOOGLE',
+                'simple_responses': {
+                    'simple_responses': [
+                        {
+                            'text_to_speech': 'Spoken simple response',
+                            'display_text': 'Displayed simple response'
+                        }
+                    ]
                 }
-            },
-            {
-                platform: 'ACTIONS_ON_GOOGLE',
-                linkOutSuggestion: {
-                    destinationName: 'Zum Artikel',
-                    uri: info.link
+            }, {
+                'platform': 'ACTIONS_ON_GOOGLE',
+                'basic_card': {
+                    'title': 'Title: this is a title',
+                    'subtitle': 'This is an subtitle.',
+                    'formatted_text': 'Body text can include unicode characters including emoji 📱.',
+                    'image': {
+                        'image_uri': 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png'
+                    },
+                    'buttons': [
+                        {
+                            'title': 'This is a button',
+                            'open_uri_action': {
+                                'uri': 'https://assistant.google.com/'
+                            }
+                        }
+                    ]
                 }
             }]
         };
